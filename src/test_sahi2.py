42e4b6de0037711cef8b0c0e15f8d6d3f0e79361
hle <hugo.lete02@gmail.com>
2026-02-23 18:32:32 +0100

    Test inférence avec Sahi (framework qui découpe l'image en tranches pour inférer)

---
+++ src/test_sahi2.py
@@ -0,0 +1,33 @@
+from sahi import AutoDetectionModel
+from sahi.predict import get_sliced_prediction
+import os
+
+detector = AutoDetectionModel()
+
+detection_model = detector.from_pretrained(
+    model_type='ultralytics',
+    model_path='../models/FinalModel.pt',
+    confidence_threshold=0.3,
+    device="cuda:0"
+)
+
+image_folder_path = "../datasets/Dataset test/"
+
+for file in os.listdir(image_folder_path):
+    if file.lower().endswith((".jpg", ".png", ".jpeg")):
+        print("Image : ", file)
+        path = os.path.join(image_folder_path, file)
+
+        result = get_sliced_prediction(
+            path,
+            detection_model,
+            slice_height=320,
+            slice_width=320,
+            overlap_height_ratio=0.3,
+            overlap_width_ratio=0.3,
+            postprocess_type="NMS",
+            postprocess_match_threshold=0.5
+        )
+
+        result.export_visuals(export_dir="outputs/")
+        print(result)
