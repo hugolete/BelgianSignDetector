0209a71d364f3d203c7ea87208a8f9db9e1c89db
hle <hugo.lete02@gmail.com>
2026-02-23 16:50:32 +0100

    Test inférence sur images de test avec tous les modèles

--- src/test.py
+++ src/test.py
@@ -3,30 +3,45 @@ import os
 import cv2
 
 
-model_path = "../models/FinalModel.pt"
 image_folder_path = "../datasets/Dataset test/"
-
-model = YOLO(model_path)
 output_folder = "predictions_test"
 
 os.makedirs(output_folder, exist_ok=True)
 
-detected_signs = 0
+model_list = [
+    "Augmentation_5epoch_best.pt",
+    "Augmentation_8epochs_best.pt",
+    "Augmentation_10epochs_best.pt",
+    "Augmentation_Epoch5.pt",
+    "Augmentation_Epoch6.pt"
+    "Augmentation_Epoch8.pt",
+    "FinalModel.pt"
+]
 
-for file in os.listdir(image_folder_path):
-    if file.lower().endswith((".jpg", ".png", ".jpeg")):
-        print("Image : ", file)
-        path = os.path.join(image_folder_path, file)
+for model_name in model_list:
+    model_path = f"../models/{model_name}"
 
-        results = model(path, conf=0.1, imgsz=640)
+    model = YOLO(model_path)
 
-        img = results[0].plot()
+    for file in os.listdir(image_folder_path):
+        detected_signs = 0
 
-        save_path = os.path.join(output_folder, file)
-        cv2.imwrite(save_path, img)
+        if file.lower().endswith((".jpg", ".png", ".jpeg")):
+            print("Image : ", file)
+            path = os.path.join(image_folder_path, file)
 
-        print("Done:", file)
-        print(file, "detections:", len(results[0].boxes))
-        detected_signs += len(results[0].boxes)
+            results = model(path, conf=0.1, imgsz=640)
 
-print("Detected signs : ",detected_signs)
+            img = results[0].plot()
+
+            save_path = os.path.join(output_folder, file)
+            cv2.imwrite(save_path, img)
+
+            print("Done:", file)
+            print(file, "detections:", len(results[0].boxes))
+            detected_signs += len(results[0].boxes)
+
+        print("Detected signs : ",detected_signs)
+
+        with open(f'{output_folder}/{model_name}.txt', 'a') as f:
+            f.write(f"Detected signs : {detected_signs}\n")
