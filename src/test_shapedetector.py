27a193e00c53e0cb952a91411df64139385450b4
hle <hugo.lete02@gmail.com>
2026-02-25 17:43:27 +0100

    Script test pour le détecteur de formes

---
+++ src/test_shapedetector.py
@@ -0,0 +1,34 @@
+from ultralytics import YOLO
+import os
+import cv2
+
+imgsz = 640
+conf_level = 0.5
+image_folder_path = "../datasets/Dataset test/"
+output_folder = "test_shapedetector"
+
+model_path = "../models/ShapeDetector.pt"
+model = YOLO(model_path)
+
+count_detections = 0
+
+for file in os.listdir(image_folder_path):
+    if file.lower().endswith((".jpg", ".png", ".jpeg")):
+        print("Image : ", file)
+        path = os.path.join(image_folder_path, file)
+
+        results = model(path, conf=conf_level, imgsz=imgsz)
+
+        img = results[0].plot()
+
+        os.makedirs(output_folder, exist_ok=True)
+        save_path = os.path.join(output_folder, file)
+        cv2.imwrite(save_path, img)
+
+        print("Done:", file)
+        print(file, "detections:", len(results[0].boxes))
+
+        count_detections += len(results[0].boxes)
+
+
+print(count_detections)
