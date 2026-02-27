d51994e1e1f9d5dad274dce106851ee4bbb58b30
hle <hugo.lete02@gmail.com>
2026-02-27 22:05:34 +0100

    Test shapedetector avec vidéo caca

--- src/test_scripts/test_shapedetector.py
+++ src/test_scripts/test_shapedetector.py
@@ -7,6 +7,7 @@ conf_level = 0.5
 image_folder_path = "../../datasets/Dataset test/"
 output_folder = "test_shapedetector"
 video_path = "../../datasets/archive/traffic-sign-to-test.mp4"
+video_path2 = "../../datasets/video1_nuit.mp4"
 
 model_path = "../../models/ShapeDetector_Kaggle_Epoch20.pt"
 model = YOLO(model_path)
@@ -14,10 +15,14 @@ model = YOLO(model_path)
 count_detections = 0
 
 #sur vidéo
-"""results = model(video_path)
-results[0].show()"""
+print("Début prédiction")
+results = model.predict(video_path2, stream=True, save=True,project="InferenceVideoTest3")
+print("Fin prédiction")
 
-# sur plusieurs images
+for _ in results:
+    pass
+
+"""# sur plusieurs images
 for file in os.listdir(image_folder_path):
     if file.lower().endswith((".jpg", ".png", ".jpeg")):
         print("Image : ", file)
@@ -37,4 +42,4 @@ for file in os.listdir(image_folder_path):
         count_detections += len(results[0].boxes)
 
 
-print(count_detections)
+print(count_detections)"""
