ac74496c448eff537eb356aa07958c883259ee94
hle <hugo.lete02@gmail.com>
2026-03-02 16:29:30 +0100

    Prédiction vidéo

--- src/test_scripts/test_video.py
+++ src/test_scripts/test_video.py
@@ -2,11 +2,9 @@ from ultralytics import YOLO
 import cv2
 
 
-def video_shape_detection(shapeDetector_path:str,video_path:str):
+def video_shape_detection_4fps(shapeDetector_path:str,video_path:str):
     shapeDetector = YOLO(shapeDetector_path)
 
-    detected_ids = set()
-    final_results = []
     max_detections_per_sec = 4
 
     # ouverture vidéo pour compter fps
@@ -15,29 +13,32 @@ def video_shape_detection(shapeDetector_path:str,video_path:str):
 
     skip_frames = int(fps / max_detections_per_sec)
     print(f"FPS : {fps}. Inférence sur 1 image toutes les {skip_frames} frames.")
-    frame_count = 0
 
+    frame_count = 0
     while vid.isOpened():
         success, frame = vid.read()
-
         if not success:
             break
 
         if frame_count % skip_frames == 0:
-            results = shapeDetector.track(frame, persist=True)
+            results = shapeDetector.predict(frame, conf=0.25, verbose=False)
 
             print(f"Inférence faite sur la frame {frame_count}")
-
-            #test
-            annotated_frame = results[0].plot()
-            print(results[0])
             print(results[0].boxes.cls)
-            cv2.imshow("Tracking", annotated_frame)
-            cv2.waitKey(0)
+            print(results[0].probs)
+            results[0].show()
 
         frame_count += 1
 
-    vid.release()  # fermeture de la vidéo
+    vid.release() # fermeture de la vidéo
+
+
+def video_shape_detection_basic(shapeDetector_path:str,video_path:str):
+    shapeDetector = YOLO(shapeDetector_path)
+    results = shapeDetector.predict(video_path, stream=True)
+
+    for _ in results:
+        print(_)
 
 
 if __name__ == '__main__':
@@ -45,4 +46,5 @@ if __name__ == '__main__':
     video_path = "../datasets/archive/traffic-sign-to-test.mp4"
     video_path2 = "../datasets/video1_nuit.mp4"
 
-    video_shape_detection(shapeDetector_path, video_path)
+    video_shape_detection_4fps(shapeDetector_path, video_path)
+    video_shape_detection_basic(shapeDetector_path, video_path)
