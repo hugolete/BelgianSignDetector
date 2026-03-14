58f2f59528b5cd030b4e6745e21753c8d98e0c14
hle <hugo.lete02@gmail.com>
2026-03-14 22:49:51 +0100

    1er test pour détection sur caméra

---
+++ src/test_scripts/test_camera.py
@@ -0,0 +1,23 @@
+from ultralytics import YOLO
+import cv2
+
+
+if __name__ == '__main__':
+    model = YOLO("../models/ShapeDetector_Kaggle_Epoch20.pt")
+
+    results = model(source=1, stream=True)  # generator of Results objects
+
+    for r in results:
+        boxes = r.boxes
+
+        if boxes.id is not None:
+            track_ids = boxes.id.int().cpu().tolist()
+            coords = boxes.xyxy.cpu().numpy()
+            class_ids = boxes.cls.int().cpu().tolist()
+            confidences = boxes.conf.cpu().tolist()
+
+            for box_id, box_coords in zip(track_ids, coords):
+                print(f"ID: {box_id} | Coords: {box_coords}")
+
+        cv2.imshow("Stream", r.plot())
+        cv2.waitKey(0)
